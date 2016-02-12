aaee39a9ffda6484ed0436986c4955a7add400ff
DetailFreak <vishsinkush@gmail.com>
2016-02-12 21:40:22 +0530

    Create Contact.php

---
+++ Website/php/Contact.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+<div id="classy"> V<small>ITAL</small>S<small>IGNS</small><sup><small>TM</small></sup> </div>
+<br>
+<ul class="navbar">
+  <li><a href="default.php">Everything</a></li>
+  <li ><a href="heart_sensor.php">Pulse</a></li>
+  <li ><a href="temperature.php">Temperature</a></li>
+  <li ><a href="about.php">What's this?</a></li>
+  <li class="active"><a href="Contact.php">Wanna talk :P ?</a></li>
+</ul>
+</body>
+</html>
