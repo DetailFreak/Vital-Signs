ccfd738284eb345bdf762d50de8acd122fe5dc66
DetailFreak <vishsinkush@gmail.com>
2016-02-12 21:36:42 +0530

    Create default.php

---
+++ Website/php/default.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+ 
+</head>
+
+<body>
+<div id="classy"> V<small>ITAL</small>S<small>IGNS</small><sup><small>TM</small></sup> </div>
+<br>
+<br>
+<ul class="navbar">
+  <li class="active"><a href="default.php">Everything</a></li>
+  <li><a href="heart_sensor.php">Pulse</a></li>
+  <li><a href="temperature.php">Humidity</a></li>
+  <li><a href="about.php">What's this?</a></li>
+  <li><a href="Contact.php">Wanna talk :P ?</a></li>
+</ul>
+<div class="myIframe">
+<iframe seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/1FUTlljL77Z6xfxlp2rW6I_6NStwKCx9IVK48kKLBicY/pubchart?oid=1271101132&amp;format=interactive"></iframe>
+</div>
+</body>
+</html>
