850acba6c4e3d5b857fef21da5af8e7d64d86c10
DetailFreak <vishsinkush@gmail.com>
2016-02-12 21:39:06 +0530

    Create temperature.php

---
+++ Website/php/temperature.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+<div id="classy"> V<small>ITAL</small>S<small>IGNS</small><sup><small>TM</small></sup> </div>
+<br>
+<br>
+<ul class="navbar">
+  <li><a href="default.php">Everything</a></li>
+  <li ><a href="heart_sensor.php">Pulse</a></li>
+  <li class="active"><a href="temperature.php">Humidity</a></li>
+  <li><a href="about.php">What's this?</a></li>
+  <li><a href="Contact.php">Wanna talk :P ?</a></li>
+</ul>
+<div class="myIframe" >
+<iframe width="788.5" height="455.0242919921875" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/1FUTlljL77Z6xfxlp2rW6I_6NStwKCx9IVK48kKLBicY/pubchart?oid=1322087532&amp;format=interactive"></iframe>
+ </div>
+</body>
+</html>
