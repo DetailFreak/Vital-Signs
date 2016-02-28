b1c47ceb167c4b8817e33b0f205c55c2f2996946
Vishal Singh Kushwaha <vishsinkush@gmail.com>
2016-02-28 18:41:06 +0530

    Update about.php

--- Website/php/about.php
+++ Website/php/about.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html>
 <head>
@@ -15,5 +16,15 @@
   <li class="active"><a href="about.php">What's this?</a></li>
   <li><a href="Contact.php">Wanna talk :P ?</a></li>
 </ul>
+<div ID="myIframe" Style="color:white;margin-left:25%;font-family:courier;">
+Hello World!
+<br>
+<br>
+This website logs sensor data in every x seconds and plots the data in a graph.
+Any harm to wild geese and envious humans is purely coincidential.
+<br>
+Website designed by Vishal Singh.
+hosted by Shozaf Abbas.  
+</div>
 </body>
 </html>
